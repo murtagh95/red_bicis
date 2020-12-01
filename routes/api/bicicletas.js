706a0ba815c877e3bafdd67dea553864a017e90d
Nicolas <nec.catalano@gmail.com>
2020-11-30 22:48:36 -0300

    Creacion y coneccion a API propia

---
+++ routes/api/bicicletas.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const bicicletaController = require('../../controllers/api/bicicletaControllerAPI');
+
+router.get('/', bicicletaController.bicicleta_list);
+router.post('/create', bicicletaController.bicicleta_create);
+router.delete('/delete', bicicletaController.bicicleta_delete);
+
+module.exports = router;
