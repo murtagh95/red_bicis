1cd2d40585a06960cefa56be865b86144ab196c0
Nicolas <nec.catalano@gmail.com>
2020-11-30 22:48:07 -0300

    Creacion y coneccion a API propia

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var bicicletasRouter = require('./routes/bicicletas');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const bicicletasRouter = require('./routes/bicicletas');
+const bicicletasAPIRouter = require('./routes/api/bicicletas');
+
 
 var app = express();
 
@@ -23,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bicicletas', bicicletasRouter);
+app.use('/api/bicicletas', bicicletasAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,8 +5,20 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Cargamos puntos de referencia
-L.marker([-32.98184263945343, -68.78846167201478]).addTo(map);
-L.marker([-32.98085265478189, -68.78311871168687]).addTo(map);
-L.marker([-32.98457853936029, -68.78554342862283]).addTo(map);
+// L.marker([-32.98184263945343, -68.78846167201478]).addTo(map);
+// L.marker([-32.98085265478189, -68.78311871168687]).addTo(map);
+// L.marker([-32.98457853936029, -68.78554342862283]).addTo(map);
 
 
+// LLamamos a nuesta API
+$.ajax({
+    dataType: "json",
+    url: 'api/bicicletas',
+    success: function (result) {
+        console.log(result);
+        // Trabajamos sobre el array que contiene bicicletas
+        result.bicicletas.forEach(bici =>{
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+});
